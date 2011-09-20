956ebd7f97484ef645b62fab2e64fbc8b8987f86
Mike Bannister <mikebannister@gmail.com>
2011-09-20 10:03:56 -0400

    added basic acceptance tests for adding maps/mappings as aids for evolving UI

--- mappable.gemspec
+++ mappable.gemspec
@@ -21,4 +21,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency "rspec-rails", "~> 2.6.1"
   s.add_development_dependency "capybara", "~> 1.1.1"
   s.add_development_dependency "mocha", "~> 0.10.0"
+  s.add_development_dependency "launchy", "~> 2.0.5"
 end
---
+++ spec/acceptance/add_mapping_spec.rb
@@ -0,0 +1,25 @@
+require 'acceptance/acceptance_helper'
+
+feature "Add mapping", %q{
+  In order to map data
+  As an system user
+  I want to add new mappings to the system
+} do
+
+  background do
+    @map = Mappable::Map.create!(from: 'legacy', to: 'current', name: 'account', attr: 'name')
+  end
+
+  scenario "Add mapping" do
+    visit "/mappable/mappings"
+
+    click_link "New Mapping"
+
+    fill_in 'From', with: 'Old account name'
+    fill_in 'To', with: 'New account name'
+    fill_in 'Map', with: @map.id
+
+    click_button "Create Mapping"
+    page.should have_content "Mapping was successfully created."
+  end
+end
